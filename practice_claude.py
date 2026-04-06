2d93be34c4dd8284e1d0253a4462290e26acc2e3
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-04-06 16:38:36 +0530

    Update practice_claude.py

--- practice_claude.py
+++ practice_claude.py
@@ -119,7 +119,23 @@ print(s1[0])
 
 Q37 [Hard]   Given a list of (item, price) tuples, find the most expensive item without using max().
 
+list_items = [('apple', 30), ('laptop', 80000), ('pen', 10), ('phone', 50000)]
 
+final_list = sorted(list_items, key = lambda x:x[1], reverse = True)
+item, price = final_list[0]
+print(item,price)
+
+# using for loop
+new_item = ''
+max_price = 0
+for item, price in final_list:
+    if price>max_price:
+        max_price = price
+        new_item = item
+    else:
+        pass
+print(new_item, max_price)
+    
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━
  7. DICTIONARIES
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━
