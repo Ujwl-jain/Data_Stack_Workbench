ba6348182229f3e3baf0506e0b6d9a5fab4809df
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-06-26 16:17:58 +0530

    Update Performing Updation Tables(All).sql

--- Performing Updation Tables(All).sql
+++ Performing Updation Tables(All).sql
@@ -3,3 +3,21 @@
 SELECT * FROM users
 SELECT * FROM admin_users
 SELECT * FROM addresses
+SELECT * FROM user_log
+
+-------------------------------
+--UPDATING TABLES USING SET AND ROLLBACK METHOD FOR THE LOVE OF THE GAME
+
+BEGIN TRANSACTION;
+
+UPDATE users SET salary = NULL WHERE id = 6
+
+ROLLBACK;
+
+SELECT * FROM users WHERE salary IS NULL
+
+-- COMMIT;
+
+
+
+
