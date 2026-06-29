e27ef86f88a7a23a8a95714960e3662d33705020
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-06-29 16:14:12 +0530

    Update Medium Query Performing.sql

--- Medium Query Performing.sql
+++ Medium Query Performing.sql
@@ -35,3 +35,43 @@ SELECT * FROM user_log;
 
 --Q10 [MEDIUM] Select name and city from users joined with addresses,
 --             for users in the city 'Pune'.
+
+	SELECT u.name, a.city
+	FROM users u
+	INNER JOIN addresses a ON u.id = a.user_id
+	WHERE a.city = 'Pune';
+
+-----------------------------------------------------------------------
+--WHERE AND FILTER
+-----------------------------------------------------------------------
+--Q16 [MEDIUM] Fetch users whose pincode is in
+--             ('411001', '400001', '110001').
+
+	SELECT u.name,u.email, a.city, a.pincode
+	FROM users u
+	INNER JOIN addresses a ON u.id = a.user_id
+	WHERE pincode IN ('411001', '400001', '110032');
+	
+--Q17 [MEDIUM] Fetch users whose name contains the word 'kumar'
+--             (case-insensitive).
+
+	SELECT * FROM users where name like '%an%'
+
+--Q18 [MEDIUM] Fetch all users who do NOT have an address record.
+
+	SELECT u.name, u.email
+	FROM users u
+	LEFT JOIN addresses a ON u.id = a.user_id
+	WHERE a.user_id IS NULL;
+
+	-- OR Subquery approach (alternative to LEFT JOIN + IS NULL)
+	SELECT * FROM users
+	WHERE id NOT IN (SELECT user_id FROM addresses);
+
+--Q19 [MEDIUM] Fetch users born between 1999-01-01 and 2000-12-31.
+
+	SELECT * FROM users WHERE date_of_birth BETWEEN '1999-01-01' and '2000-12-31';
+
+--Q20 [MEDIUM] Fetch users whose name is exactly 5 characters long.
+	
+	SELECT * FROM users WHERE LEN(name) = 5;
