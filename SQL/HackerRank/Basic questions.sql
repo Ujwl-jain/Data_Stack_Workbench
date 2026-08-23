140c2d1f59c274d2f8e66a06a86847558f085886
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-08-23 11:21:03 +0530

    Update Basic questions.sql

--- SQL/HackerRank/Basic questions.sql
+++ SQL/HackerRank/Basic questions.sql
@@ -78,3 +78,18 @@ LIMIT 1;
   FROM STATION 
   WHERE LOWER(LEFT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u') 
   AND LOWER(RIGHT(CITY, 1)) IN ('a', 'e', 'i', 'o', 'u');
+
+-----------------------
+-- Q6. Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION
+WHERE LOWER(LEFT(CITY, 1)) NOT IN ('a','e','i', 'o', 'u');
+
+----------
+-- Q7. Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION
+WHERE LOWER(RIGHT(CITY, 1)) NOT IN ('a','e','i', 'o', 'u');
+
+---------
+-- Q8. Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
