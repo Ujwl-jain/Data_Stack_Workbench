08b79abae2fb42715a6100a31e4a9830ced822b4
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-08-24 15:40:04 +0530

    Update Easy.py

--- Python/HackerRank/Easy.py
+++ Python/HackerRank/Easy.py
@@ -75,3 +75,77 @@ if __name__ == '__main__':
     line = input()
     result = split_and_join(line)
     print(result)
+
+# ---------
+# Q5. Consider a list (list = []). You can perform the following commands:
+
+# insert i e: Insert integer  at position .
+# print: Print the list.
+# remove e: Delete the first occurrence of integer .
+# append e: Insert integer  at the end of the list.
+# sort: Sort the list.
+# pop: Pop the last element from the list.
+# reverse: Reverse the list.
+# Initialize your list and read in the value of n followed by n lines of commands where each command will be of the 7 types listed above. Iterate through each command in order and perform the corresponding operation on your list.
+
+#     Input Format
+
+# The first line contains an integer, n , denoting the number of commands.
+# Each line i of the n subsequent lines contains one of the commands described above.
+
+# Constraints
+
+# The elements added to the list must be integers.
+# Output Format
+
+# For each command of type print, print the list on a new line.
+
+# Sample Input 0
+
+# 12
+# insert 0 5
+# insert 1 10
+# insert 0 6
+# print
+# remove 6
+# append 9
+# append 1
+# sort
+# print
+# pop
+# reverse
+# print
+# Sample Output 0
+
+# [6, 5, 10]
+# [1, 5, 9, 10]
+# [9, 5, 1]
+    
+if __name__ == '__main__':
+    N = int(input())
+    
+lst = []
+for i in range(N):
+    com = input()
+    command = com.split()
+    if 'insert' in command[0]:
+       lst.insert(int(command[1]),int(command[2])) 
+       
+    elif 'print' in command[0]:
+        print(lst)
+        
+    elif 'remove' in command[0]:
+        lst.remove(int(command[1]))
+        
+    elif 'append' in command[0]:
+        lst.append(int(command[1]))
+        
+    elif 'sort' in command[0]:
+        lst.sort()
+        
+    elif 'pop' in command[0]:
+        lst.pop()
+        
+    elif 'reverse' in command[0]:
+        lst.reverse()
+        
