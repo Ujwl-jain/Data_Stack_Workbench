68fb5ad7f622a77923f774fc67fb7dfec1979b24
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-08-24 12:57:41 +0530

    Update Easy.py

--- Python/HackerRank/Easy.py
+++ Python/HackerRank/Easy.py
@@ -61,3 +61,17 @@ for n, mrk in student_marks.items():
         
         
 print(average_marks[query_name])
+
+---------
+# Q4. In Python, a string can be split on a delimiter. You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.
+# Input: this is a string   
+
+def split_and_join(line):
+    split_line = line.split(' ')
+    return '-'.join(split_line)
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
