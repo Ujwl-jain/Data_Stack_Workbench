e07e7f0255a488d1c49db660715e9206e9ad83a6
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-08-26 12:32:57 +0530

    Update Basic questions.sql

--- SQL/HackerRank/Basic questions.sql
+++ SQL/HackerRank/Basic questions.sql
@@ -1,4 +1,4 @@
--- Current table:
+-- Current table: STATION
 
 -- id NUMBER
 -- city VARCHAR(21)
@@ -155,3 +155,36 @@ SELECT ROUND(SUM(LAT_N),2) AS lat,
 ROUND(SUM(LONG_W),2) AS lon
 FROM STATION
 
+--------------------------------------------------------------------------------------
+-- Table: CITY
+--   ID = NUMBER
+--   NAME = VARCHAR2(17)
+--   COUNTRYCODE = VARCHAR2(3)
+--   DISTRICT = VARCHAR2(20)
+--   POPULATION = NUMBER
+-- Q16.  Query a count of the number of cities in CITY having a Population larger than .
+SELECT COUNT(DISTINCT(NAME)) FROM CITY
+WHERE POPULATION > 100000
+
+---------
+-- Q17. Query the total population of all cities in CITY where District is California.
+
+SELECT SUM(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+---------
+-- Q18. Query the average population of all cities in CITY where District is California.
+
+SELECT AVG(POPULATION) FROM CITY
+WHERE DISTRICT = 'California';
+
+---------
+-- Q19. Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+  SELECT ROUND(AVG(POPULATION)) FROM CITY;
+
+---------
+-- Q20. Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+
+SELECT SUM(POPULATION) FROM CITY
+WHERE COUNTRYCODE = 'JPN';
