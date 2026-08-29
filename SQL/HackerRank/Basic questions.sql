c7391165d81e0a71835cdc82370bf77bce73bcd2
Ujjwal Jain <jain24ujjwal@gmail.com>
2026-08-29 16:15:42 +0530

    Update Basic questions.sql

--- SQL/HackerRank/Basic questions.sql
+++ SQL/HackerRank/Basic questions.sql
@@ -229,8 +229,43 @@ ON COUNTRY.CODE = CITY.COUNTRYCODE
 GROUP BY COUNTRY.CONTINENT;
 
 ---------
--- 23. Query the difference between the maximum and minimum populations in CITY.
+-- Q23. Query the difference between the maximum and minimum populations in CITY.
 
 SELECT (MAX(POPULATION) - MIN(POPULATION))
 FROM CITY
 
+---------
+-- Q24. Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT SUM(CITY.POPULATION) FROM CITY
+LEFT JOIN COUNTRY ON
+CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE COUNTRY.CONTINENT = 'ASIA'
+
+---------
+
+-- Q25. We define an employee's total earnings to be their monthly (month * salary) worked, and the maximum total earnings to be the maximum total earnings for any employee in the 
+-- Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+-- Then print these values as 2 space-separated integers.
+
+  -- Table:
+  -- Column         Type
+  -- employee_id  Integer
+  -- name         String
+  -- months       Integer
+  -- salary       Integer
+  
+SELECT (months * salary), COUNT(*)
+FROM Employee
+GROUP BY (months * salary)
+ORDER BY (months * salary) DESC
+LIMIT 1;
+
+-- ---------
+-- Q26. Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's 0 key was broken until 
+-- after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+-- Write a query calculating the amount of error (i.e.: actual - miscalculated average monthly salaries), and round it up to the next integer.
+
+SELECT CEIL(AVG(salary) - AVG(REPLACE(salary, '0', '')))
+FROM EMPLOYEES;
